0d00da33e04444047acf9bdbb25e03b74466c30f
oizovita <oizovita@yahoo.com>
2021-05-05 01:39:17 +0300

    add carausel

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(function(){
+$(function () {
+    $(document).ready(function () {
+        const owl = $("#owl-example");
 
+        owl.owlCarousel({
+            loop: true,
+            items: 1,
+            slideSpeed: 300,
+            paginationSpeed: 400,
+            singleItem: true
+        });
+
+        $(".next").click(function () {
+            console.log('asd')
+            owl.trigger('next.owl.carousel', [600]);
+        })
+        $(".prev").click(function () {
+            owl.trigger('prev.owl.carousel', [600]);
+        })
+    });
 });
